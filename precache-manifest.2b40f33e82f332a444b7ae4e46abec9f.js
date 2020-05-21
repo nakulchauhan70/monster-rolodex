d913a810c0fe37b18a57dfd7825460bcfce56c0a
NakulGitHub <nkul.ic20@gmail.com>
2020-05-21 10:18:36 +0530

    Updates

---
+++ precache-manifest.2b40f33e82f332a444b7ae4e46abec9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddbba5a0dadc20537416490db3bb0f0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0750e663d017561b2c10",
+    "url": "/monster-rolodex/static/css/main.90820270.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0750e663d017561b2c10",
+    "url": "/monster-rolodex/static/js/main.672b47ff.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
